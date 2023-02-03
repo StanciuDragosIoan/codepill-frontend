032682c9e0e23ab4eabb2c6d96505216a5b32e6c
stanciudragosioan <stanciudragosioan92@gmail.com>
2023-02-03 18:11:38 +0200

    feat: add initial pages

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,7 @@
+export default function IndividualPost() {
+  return (
+    <>
+      <h1>IndividualPostPage here</h1>
+    </>
+  );
+}
---
+++ pages/topics.tsx
@@ -0,0 +1,7 @@
+export default function TopicsPage() {
+  return (
+    <>
+      <h1>TopicsPage here</h1>
+    </>
+  );
+}
